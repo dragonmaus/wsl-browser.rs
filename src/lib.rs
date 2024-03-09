df6d7f4b3e0f3671750e3dafc3dcfbf4c5a2d4e5
David Wildasin <dragonmaus@posteo.net>
2024-03-09 00:02:24 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::{
+    env, error, fs,
+    io::{Error, ErrorKind},
+    process::{Command, Stdio},
+    result::Result,
+};
+
+use url::Url;
+
+pub fn open(spec: &str) -> Result<i32, Box<dyn error::Error>> {
+    match Command::new(get_external_browser())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .arg(get_url(spec)?)
+        .status()
+    {
+        Ok(status) if status.success() => Ok(0),
+        Ok(status) => Err(Box::new(Error::new(
+            ErrorKind::Other,
+            match status.code() {
+                Some(c) => format!("process returned status code {}", c),
+                None => "process was terminated by a signal".into(),
+            },
+        ))),
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+fn fixup_url(url: Url) -> Result<String, Box<dyn error::Error>> {
+    Ok(if url.scheme() == "file" {
+        let mut path = wslpath::wsl_to_windows(url.path())?;
+
+        if path.starts_with("//wsl$/") {
+            // firefox requires an extra / here for some reason
+            path.insert(0, '/');
+        }
+        path.insert_str(0, "file://");
+
+        path
+    } else {
+        url.into()
+    })
+}
+
+fn force_url(spec: &str) -> Result<Url, Box<dyn error::Error>> {
+    Ok(match Url::parse(spec) {
+        Ok(u) => u,
+        Err(_e) => Url::parse("file:///")?.join(&fs::canonicalize(spec)?.to_string_lossy())?,
+    })
+}
+
+fn get_external_browser() -> String {
+    match env::var("EXTERNAL_BROWSER") {
+        Ok(v) => v,
+        Err(_) => "external-browser".into(),
+    }
+}
+
+fn get_url(spec: &str) -> Result<String, Box<dyn error::Error>> {
+    fixup_url(force_url(spec)?)
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use getopt::Opt;
+
+program::main!("wsl-browser");
+
+fn usage_line(program_name: &str) -> String {
+    format!("Usage: {} [URI]", program_name)
+}
+
+fn print_usage(program_name: &str) {
+    println!("{}", usage_line(program_name));
+    println!();
+    println!("  -h   display this help");
+}
+
+fn program(name: &str) -> program::Result {
+    let mut args = program::args();
+    let mut opts = getopt::Parser::new(&args, "h");
+
+    loop {
+        match opts.next().transpose()? {
+            None => break,
+            Some(opt) => match opt {
+                Opt('h', None) => {
+                    print_usage(name);
+                    return Ok(0);
+                },
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    let mut args = args.split_off(opts.index());
+
+    if args.is_empty() {
+        args.push(String::from("."));
+    }
+
+    for arg in args {
+        wsl_browser::open(&arg)?;
+    }
+
+    Ok(0)
+}
